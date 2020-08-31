1fd7d3ba0d9c7f63f98691cf8d9a45495203d007
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-08-31 15:27:19 +0900

    namechange

---
+++ ThrottleSchedulerService/ThrottleSchedulerService/Service1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Timers;
+
+namespace ThrottleSchedulerService
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        essentials ess = new essentials();
+
+        int count = 0;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ess.WriteLog("service started");
+            timer.Elapsed += new ElapsedEventHandler(OnTimerCount);
+            timer.Interval = 1000;
+            timer.Enabled = true;
+        }
+
+        private void OnTimerCount(Object src, ElapsedEventArgs args) {
+            ess.WriteLog("service running " + count++ + " times");
+        }
+
+        protected override void OnStop()
+        {
+            ess.WriteLog("service stopped");
+        }
+    }
+}
---
+++ ThrottleSchedulerService/ThrottleSchedulerService/essentials.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace ThrottleSchedulerService
+{
+    class essentials
+    {
+        public void WriteLog(string msg) {
+            string folderpath = AppDomain.CurrentDomain.BaseDirectory + "\\logs";
+            if (!Directory.Exists(folderpath)) Directory.CreateDirectory(folderpath);
+
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\logs\\" +
+                DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath)) 
+                using (StreamWriter sw = File.CreateText(filepath)) sw.WriteLine(DateTime.Now + ": " + msg);
+            else
+                using (StreamWriter sw = File.AppendText(filepath)) sw.WriteLine(DateTime.Now + ": " + msg);
+        }
+    }
+}
