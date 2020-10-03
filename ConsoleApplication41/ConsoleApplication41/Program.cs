39bdafa6df6f9628a7e64c18cfd4e01c58c65be3
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-10-04 03:39:15 +0900

    change to a console application
    services cant have access to hwnd due to security constraints

---
+++ ConsoleApplication41/ConsoleApplication41/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Diagnostics;
+
+using System.IO;
+using System.Management;
+using OpenHardwareMonitor.Hardware;
+
+using System.Runtime.InteropServices;
+
+using System.Threading;
+
+namespace ThrottleSchedulerService
+{
+    class Program
+    {
+       
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+
+        System.Timers.Timer timer = new System.Timers.Timer();
+        essentials ess = new essentials();
+        int msec = 1000;
+        ThrottleScheduler ts;
+        Stopwatch stopwatch;
+
+        private void OnTimerCount(Object src, ElapsedEventArgs args)
+        {
+            //actual sync
+            stopwatch.Restart();
+            timer.Enabled = false;
+
+            ts.mainflow();
+
+            //actual sync
+            stopwatch.Stop();
+            double interval = msec - stopwatch.ElapsedMilliseconds;
+            if (interval < 0.1) interval = 0.1;
+            timer.Interval = interval;
+            timer.Enabled = true;
+
+            ess.WriteLog("system elapsed time = " + stopwatch.ElapsedMilliseconds + ", service timer interval = " + timer.Interval);
+        }
+
+        public void service() {
+            ts = new ThrottleScheduler(msec);
+            stopwatch = Stopwatch.StartNew();
+
+            timer.Elapsed += new ElapsedEventHandler(OnTimerCount);
+            timer.Interval = msec;
+            timer.Enabled = true;
+
+            timer.Start();
+        }
+
+        public static void Main() {
+            
+            var handle = GetConsoleWindow();
+
+            //ShowWindow(handle, SW_HIDE); // To hide
+
+            var hello = new Program();
+            hello.service();
+           
+            //keep main thread running
+            while (true) { Thread.Sleep(int.MaxValue); }
+        
+        }
+    }
+
+}
