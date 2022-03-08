561201c20a082bcb1f6b394f39ed89718f3433ac
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2022-03-08 14:45:13 +0900

    Update gpu_scheduler.ps1
    better way to detect game is checking copy percentage

--- gpu_scheduler.ps1
+++ gpu_scheduler.ps1
@@ -1,7 +1,5 @@
 #user config
-$totaloffset_up = 20
-$totaloffset_down = 10
-$limit = 50
+$limit = 1
 $sleeptime = 5
 
 #gpu config
@@ -9,7 +7,6 @@ $clockoffset = -950
 $memoffset = -1000
 
 #internal stuff
-$total = 0
 $delta = 0
 $switching = 1
 #logging stuff
@@ -22,22 +19,17 @@ function msg ([string]$setting_string){
 
 
 while($true){
-	$delta = $(Get-WmiObject -class Win32_Processor)['LoadPercentage']
-	if ($total -le $delta -And $total -le 100){
-		$total += $totaloffset_up
-	}elseif($total -gt $delta -And $total -gt 0){
-		$total -= $totaloffset_down
-	}
-	msg("total: " + $total + ", delta: " + $delta)
-	if ($total -gt $limit -Or $delta -gt $limit){
+	$delta = (((Get-Counter "\GPU Engine(*engtype_Copy)\Utilization Percentage").CounterSamples | where CookedValue).CookedValue | measure -sum).sum
+	msg("delta: " + $delta)
+	if ($delta -ge $limit){
 		if ($switching -eq 0){
 			C:\nvidiaInspector\nvidiaInspector.exe -setBaseClockOffset:0,0,$clockoffset -setMemoryClockOffset:0,0,$memoffset
 			$switching = 1
 			msg("gpu is in game mode")
 		}
-	}elseif($total -le $limit -And $delta -le $limit){
+	}elseif($delta -lt $limit){
 		if($switching -eq 1){
-			C:\nvidiaInspector\nvidiaInspector.exe -setBaseClockOffset:0,0,0 -setMemoryClockOffset:0,0,0
+			C:\nvidiaInspector\nvidiaInspector.exe -setBaseClockOffset:0,0,0 -setMemoryClockOffset:0,0,$memoffset
 			$switching = 0
 			msg("gpu is sleeping")
 		}
