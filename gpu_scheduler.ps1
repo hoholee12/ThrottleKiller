96eb59487465272ca222a990d3b9f67f59c08be7
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2022-03-09 01:13:37 +0900

    Update gpu_scheduler.ps1
    1 delay safeguard for burst

--- gpu_scheduler.ps1
+++ gpu_scheduler.ps1
@@ -9,6 +9,7 @@ $memoffset = -1000
 #internal stuff
 $delta = 0
 $switching = 1
+$switchdelay = 0
 #logging stuff
 function msg ([string]$setting_string){
 	#print by date and time
@@ -22,17 +23,19 @@ while($true){
 	$delta = (((Get-Counter "\GPU Engine(*engtype_Copy)\Utilization Percentage").CounterSamples | where CookedValue).CookedValue | measure -sum).sum
 	msg("delta: " + $delta)
 	if ($delta -ge $limit){
-		if ($switching -eq 0){
+		if ($switching -eq 0 -And $switchdelay -eq 1){
 			C:\nvidiaInspector\nvidiaInspector.exe -setBaseClockOffset:0,0,$clockoffset -setMemoryClockOffset:0,0,$memoffset
 			$switching = 1
 			msg("gpu is in game mode")
 		}
+		$switchdelay = 1
 	}elseif($delta -lt $limit){
 		if($switching -eq 1){
 			C:\nvidiaInspector\nvidiaInspector.exe -setBaseClockOffset:0,0,0 -setMemoryClockOffset:0,0,$memoffset
 			$switching = 0
 			msg("gpu is sleeping")
 		}
+		$switchdelay = 0
 	}
 	start-sleep $sleeptime
 }
