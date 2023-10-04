ed21de390f334c64f3696e9544aa8c766a0f314a
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2023-10-04 16:14:04 +0900

    Update gpu_scheduler.ps1

--- gpu_scheduler.ps1
+++ gpu_scheduler.ps1
@@ -20,7 +20,7 @@ $delaycpu = 1			# delay from sudden gpulimit (only under deltabias)
 $delaygpu = 0			# delay from sudden gpudefault (only under deltabias)
 $throttlechange = 5		# delay from sudden throttle clear (will also be used for reducing frequent switches)
 $isdebug = $false		# dont print debug stuff
-$cpulim = 50			# powersave feature. set it 100 for max performance.
+$cpulim = 100			# powersave feature. set it 100 for max performance.
 $deltalim = 50			# arbitrary gpudelta limit(in case delta doesnt work)
 
 # for gpulimit
@@ -32,7 +32,7 @@ $defclockoffset = 0
 $defmemoffset = 0
 
 
-$opport = 100 - [math]::ceiling(100 / (Get-WmiObject Win32_Processor).NumberOfCores)
+$boost = [math]::ceiling(100 / (Get-WmiObject Win32_Processor).NumberOfCores)
 
 # better cpu scheduler tuning
 # powersettings(HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Power\PowerSettings)
@@ -41,7 +41,6 @@ $processor_power_management_guids = @{
 "0cc5b647-c1df-4637-891a-dec35c318583" = 100
 "12a0ab44-fe28-4fa9-b3bd-4b64f44960a6" = 15			# processor low threshold; upper this for batterylife
 "40fbefc7-2e9d-4d25-a185-0cfd8574bac6" = 1			# processor low plan(0:normal, 1:step, 2:rocket)
-"45bcc044-d885-43e2-8605-ee0ec6e96b59" = $opport	# percentage of cores to opportunistically boost above high threshold
 "465e1f50-b610-473a-ab58-00d1077dc418" = 2			# processor high plan(0:normal, 1:step, 2:rocket)
 "4d2b0152-7d5c-498b-88e2-34345392a2c5" = 15
 "94d3a615-a899-4ac5-ae2b-e4d8f634367f" = 1
@@ -52,6 +51,7 @@ $guid0 = 'scheme_current'		# powerplan(HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\
 $guid1 = '54533251-82be-4824-96c1-47b60b740d00'		# processor power management
 $guid2 = 'bc5038f7-23e0-4960-96da-33abaf5935ec'		# processor high clockspeed limit
 $guid3 = '893dee8e-2bef-41e0-89c6-b55d0929964c'		# processor high2 clockspeed limit
+$guidx = '45bcc044-d885-43e2-8605-ee0ec6e96b59'		# percentage of cores to opportunistically boost above high threshold
 
 $guid4 = '44f3beca-a7c0-460e-9df2-bb8b99e0cba6'		# intel graphics power management
 $guid5 = '3619c3f2-afb2-4afc-b0e9-e7fef372de36'		# submenu of intel graphics power management
@@ -103,6 +103,7 @@ $global:currpwr_v = 100 * $global:currpwr_n
 $global:currpwr = $global:currpwr_v / $global:currpwr_n
 $global:cputhrottle = 0
 $global:cpulimitval = 100
+$global:cpuboost = $boost
 $global:throttle_str = ""
 $global:prev_process = ""
 $global:status = 0			# 0 = gpudefault, 1 = gpulimit
@@ -339,6 +340,9 @@ function gpudefault{
 
 function cpulimit($idleness){
 	$prevlim = $global:cpulimitval
+	$prevboost = $global:cpuboost
+	
+	#cpulim
 	if($idleness -eq 1){
 		$global:cpulimitval = $cpulim
 	}
@@ -348,15 +352,33 @@ function cpulimit($idleness){
 	else{
 		$global:cpulimitval = 99
 	}
-	if($prevlim -ne $global:cpulimitval){
-		msg("cpulimit adjusted to "+$global:cpulimitval)
+	#cpuboost
+	if($idleness -eq 1){
+		$global:cpuboost = $boost
+	}
+	elseif($global:cputhrottle -ne 2){
+		$global:cpuboost = 100
+	}
+	else{
+		$global:cpuboost = $boost
+	}
+	
+	#apply
+	if($prevlim -ne $global:cpulimitval -Or $prevboost -ne $global:cpuboost){
 		powercfg /setdcvalueindex $guid0 $guid1 $guid2 $global:cpulimitval
 		powercfg /setacvalueindex $guid0 $guid1 $guid2 $global:cpulimitval
+		powercfg /setdcvalueindex $guid0 $guid1 $guidx $global:cpuboost
+		powercfg /setacvalueindex $guid0 $guid1 $guidx $global:cpuboost
+		msg("cpulimit adjusted to "+$global:cpulimitval+", boost to "+$global:cpuboost)
 	}
 }
 
 # first time
-cpulimit(1)
+powercfg /setdcvalueindex $guid0 $guid1 $guid2 $global:cpulimitval
+powercfg /setacvalueindex $guid0 $guid1 $guid2 $global:cpulimitval
+powercfg /setdcvalueindex $guid0 $guid1 $guidx $global:cpuboost
+powercfg /setacvalueindex $guid0 $guid1 $guidx $global:cpuboost
+msg("cpulimit adjusted to "+$global:cpulimitval+", boost to "+$global:cpuboost)
 nvidiaInspector -setBaseClockOffset:0,0,$defclockoffset -setMemoryClockOffset:0,0,$defmemoffset
 
 while($true){
